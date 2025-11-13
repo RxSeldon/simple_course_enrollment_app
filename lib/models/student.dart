dcdb73d4aeafc7fba43e54c7f0b6ef8be3567fc4
RSeldon06 <reinerseldon.delacerna.20@usjr.edu.ph>
2025-11-13 17:37:52 +0800

    defined student model class

---
+++ lib/models/student.dart
@@ -0,0 +1,27 @@
+class Student {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String studentID;
+
+  Student({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.studentID,
+  });
+
+  Student copyWith({
+    String? firstName,
+    String? lasatName,
+    String? email,
+    String? studentID,
+  }) {
+    return Student(
+      firstName: firstName ?? this.firstName,
+      lastName: lasatName ?? this.lastName,
+      email: email ?? this.email,
+      studentID: studentID ?? this.studentID,
+    );
+  }
+}
