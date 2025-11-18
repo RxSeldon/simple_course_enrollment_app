cde74499f76a444bb3bce05b1e88280a0acb2d80
RSeldon06 <reinerseldon.delacerna.20@usjr.edu.ph>
2025-11-18 17:21:10 +0800

    added enrollment_provider.dart

---
+++ lib/providers/enrollment_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:simple_course_enrollment_app/models/student.dart';
+import 'package:simple_course_enrollment_app/models/course.dart';
+
+class EnrollmentProvider extends ChangeNotifier {
+  Student? _student;
+  Course? _selectedCourse;
+
+  Student? get student => _student;
+  Course? _selectedCourse;
+
+  Student? get student => _selectedCourse;
+
+  void setStudent(Student student) {
+    _student = student;
+    notifyListeners();
+  }
+
+  void setCourse(Course course) {
+    _selectedCourse = course;
+    notifyListeners();
+  }
+
+  void clear() {
+    _student = null;
+    _selectedCourse = null;
+    notifyListeners();
+  }
+
+  bool get isComplete => _student != null && _selectedCourse != null;
+}
